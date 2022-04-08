5f1287e4c6d4c5e9634e47323c32e772ebd42f0f
Jay Prakash Sharma <jayprakashsharma7@icloud.com>
2022-04-08 14:01:39 +0530

    Initial Commit

---
+++ Movie list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie list
+//
+//  Created by Jay Prakash Sharma on 08/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie list/Movie_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_listApp.swift
+//  Movie list
+//
+//  Created by Jay Prakash Sharma on 08/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
